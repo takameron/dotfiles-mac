02309ebd6531de91bb3e80737e9cff428e8b9db6
takameron <contact@takameron.info>
2021-02-28 16:42:47 +0900

    fix setting current version

--- asdf/install.sh
+++ asdf/install.sh
@@ -22,14 +22,14 @@ asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
 # Install Node.js
 asdf install nodejs latest
-asdf global nodejs "$(asdf list nodejs | tail -1)"
+asdf global nodejs "$(asdf list nodejs | tail -1 | sed -e 's/ //g')"
 
 # === asdf-python ===
 # Install plugin
 asdf plugin-add python https://github.com/danhper/asdf-python
 # Install Python
 asdf install python latest
-asdf global python "$(asdf list python | tail -1)"
+asdf global python "$(asdf list python | tail -1 | sed -e 's/ //g')"
 
 # === asdf-ruby ===
 # Requirements(optional, but recommended)
@@ -39,6 +39,6 @@ echo "export RUBY_CONFIGURE_OPTS=\"--with-openssl-dir=\"$(brew --prefix openssl@
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
 # Install Ruby
 asdf install ruby latest
-asdf global ruby "$(asdf list ruby | tail -1)"
+asdf global ruby "$(asdf list ruby | tail -1 | sed -e 's/ //g')"
 
 echo "👍 asdf install is done!"
