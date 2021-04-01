f23b0e989c340ff57c2414d0f21facc4d77b5ad9
takameron <contact@takameron.info>
2021-04-02 08:19:29 +0900

    fix: load zsh-users/zsh-completions

--- dotfiles/.plugins.zsh
+++ dotfiles/.plugins.zsh
@@ -4,8 +4,13 @@ zinit ice wait'!0'; zinit light zsh-users/zsh-autosuggestions # 履歴補完
 zinit ice wait'!0'; zinit light zdharma/fast-syntax-highlighting # シンタックスハイライト
 zinit ice wait'!0'; zinit light zdharma/history-search-multi-word # Ctrl-Rでコマンド履歴を検索
 
-zinit ice wait'!0'; zinit light zsh-users/zsh-completions # 補完（多数のコマンドに対応）
 zinit ice wait'!0'; zinit light paulirish/git-open # GitHubで開く($git open)
+zinit ice wait'!0'; zinit light zsh-users/zsh-completions # 補完（多数のコマンドに対応）
+autoload -Uz compinit && compinit
+## 補完で小文字でも大文字にマッチさせる
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+## 補完候補を一覧表示したとき、Tabや矢印で選択できるようにする
+zstyle ':completion:*:default' menu select=1
 
 zinit ice compile'(pure|async).zsh' pick'async.zsh' src'pure.zsh'
 zinit light sindresorhus/pure
