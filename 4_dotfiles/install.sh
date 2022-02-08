7a3528d4c73cada136e7bb87df9f9ecebcccccd4
takameron <tech@takameron.info>
2022-02-08 10:30:59 +0900

    fix Non-POSIX compliant

--- 4_dotfiles/install.sh
+++ 4_dotfiles/install.sh
@@ -5,7 +5,7 @@ basename -a "$PWD"/dotfiles/.??* | xargs -I{} ln -sfv "$PWD"/dotfiles/{} ~/{}
 
 # Zinit (Zsh plugin manager)
 sh -c "$(curl -fsSL https://git.io/zinit-install)"
-echo ". "$PWD"/4_dotfiles/.plugins.zsh" >> ~/.zshrc
+echo ". $PWD/4_dotfiles/.plugins.zsh" >> ~/.zshrc
 
 # Delete .DS_Store
 echo "alias dsstore=\"find . -name '*.DS_Store' -type f -ls -delete\"" >> ~/.zshrc
